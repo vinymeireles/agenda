e2328fc54d2e58f68c7fe666e47c4f659ff926ff
Viny <viniciusmeireles@gmail.com>
2022-01-24 19:07:31 -0300

    Projeto do App Agenda em Python com Django

---
+++ core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+#classe que será migrada para o banco de dados como tabela
+class Evento(models.Model): 
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True) #insere a hora atual no campo
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) #usuario com atributo de chave estrangeira
+
+    class Meta:
+        db_table = 'evento' #table passara a ser chamar 'evento'
+
+    def __str__(self):
+        return self.titulo #atualizando o nome do evento object para o titulo
